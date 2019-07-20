d9275f9542e1a9cfa59393f576f554856a27261c
Hunter Chen <1480811935@qq.com>
2019-07-20 16:38:16 +0800

    first add

---
+++ src/main/java/Spider.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Created by Hunter on 7/20/2019
+ * 注意保持ChromeDriver版本和Chrome版本一致，否则会报错
+ */
+public class Spider {
+    public static void main(String[] args) {
+        String path="WebSpider\\src\\main\\resources\\chromedriver.exe";
+        //设置WebDriver路径
+        System.setProperty("webdriver.chrome.driver", path);
+        //创建WebDriver
+        WebDriver  webDriver=new ChromeDriver();
+        //打开浏览器地址
+        webDriver.get("https://diannao.jd.com/");
+    }
+}
