b026d3d5d9804bb9c0bf724016ecc30f45954310
Hunter Chen <1480811935@qq.com>
2019-07-22 15:46:50 +0800

    modified

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## 网路爬虫
 利用Java实现网络爬虫
 
-## 需要工具
+## 工具
 [ChromeDriver](https://sites.google.com/a/chromium.org/chromedriver/)
 
 [Selenium](https://docs.seleniumhq.org/)  
--- src/main/java/Spider.java
+++ src/main/java/Spider.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 /**
@@ -13,6 +15,16 @@ public class Spider {
         //创建WebDriver
         WebDriver  webDriver=new ChromeDriver();
         //打开浏览器地址
-        webDriver.get("https://diannao.jd.com/");
+        webDriver.get("https://www.lagou.com/jobs/list_Java?isSchoolJob=1#/");
+        //选择
+        choseElement(webDriver, "工作性质：", "应届");
+        choseElement(webDriver, "学历要求：", "硕士");
+        choseElement(webDriver, "行业领域：", "移动互联网");
+    }
+    //通过Xpath选中元素
+    private static void choseElement(WebDriver webDriver, String title, String chose) {
+        WebElement titleElement = webDriver.findElement(By.xpath("//li[@class='multi-chosen']//span[contains(text(),'" + title + "')]"));
+        WebElement choseElement = titleElement.findElement(By.xpath("//a[contains(text(),'" + chose + "')]"));
+        choseElement.click();
     }
 }
